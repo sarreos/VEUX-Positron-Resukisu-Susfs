399e5dc797f5c588c3e252317b37b9141a4b8a7e
sarreos <47927259+sarreos@users.noreply.github.com>
2026-02-11 18:45:36 +0100

    Fix line endings for shell scripts in build.sh
    Added a command to remove carriage return characters from shell scripts.

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ wget -O- https://github.com/vc-teahouse/Baseband-guard/raw/main/setup.sh | bash
 sed -i '/^config LSM$/,/^help$/{ /^[[:space:]]*default/ { /baseband_guard/! s/selinux/selinux,baseband_guard/ } }' security/Kconfig
 # resukisu
 curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash
+find . -type f -name "*.sh" -exec sed -i 's/\r$//' {} +
 bash backport_patches.sh
 bash backport_selinux_patches.sh
 bash susfs_inline_hook_patches.sh
@@ -16,4 +17,5 @@ make -j$(nproc --all) O=out \
     CLANG_TRIPLE=aarch64-linux-gnu- \
     CROSS_COMPILE=aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+
 
